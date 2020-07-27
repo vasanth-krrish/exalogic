18df6ac3776c5e9bba524098049060c0bba84a30
vasanth-krrish <vasanthkrrish96@gmail.com>
2020-07-27 20:21:06 +0530

    init

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < Devise::SessionsController
   def create
     super
     if current_user.has_role?(:admin)
-      flash[:notice] = "You've logged in as admin"
+      flash[:notice] = "You've logged in as admin."
     end
   end
 end
